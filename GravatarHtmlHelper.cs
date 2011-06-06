e3a5a996d6b2600da889d69233b56a7ed84b7477
Joar Langaard <joar.langaard@gmail.com>
2011-06-06 21:50:13 +0200

    GravatarHtmlHelper for ASP.NET MVC

---
+++ GravatarHtmlHelper.cs
@@ -0,0 +1,90 @@
+﻿namespace System.Web.Helpers
+{
+	using System.Collections.Generic;
+	using System.Security.Cryptography;
+	using System.Text;
+	using System.Web;
+	using System.Web.Mvc;
+	using System.Web.Routing;
+
+	public static class GravatarHtmlHelper
+	{
+		public static HtmlString Gravatar(this HtmlHelper html, string email)
+		{
+			return GetImage(GetGravatar(email));
+		}
+
+		public static HtmlString Gravatar(this HtmlHelper html, string email, object gravatarAttributes)
+		{
+			return GetImage(GetGravatar(email, gravatarAttributes));
+		}
+
+		public static HtmlString Gravatar(this HtmlHelper html, string email, object gravatarAttributes, object htmlAttributes)
+		{
+			return GetImage(GetGravatar(email, gravatarAttributes), htmlAttributes);
+		}
+
+		private static HtmlString GetImage(string source)
+		{
+			return GetImage(source, null);
+		}
+
+		private static HtmlString GetImage(string source, object htmlAttributes)
+		{
+			IDictionary<string, object> attributes =
+				(htmlAttributes == null
+					? new RouteValueDictionary()
+					: new RouteValueDictionary(htmlAttributes));
+
+			var tagBuilder = new TagBuilder("img");
+			tagBuilder.MergeAttribute("src", source);
+			tagBuilder.MergeAttributes(attributes);
+
+			return new HtmlString(tagBuilder.ToString(TagRenderMode.SelfClosing));
+		}
+
+		private static string GetGravatar(string email)
+		{
+			return string.Format("http://www.gravatar.com/avatar/{0}", Hash(email));
+		}
+
+		private static string GetGravatar(string email, object gravatarAttributes)
+		{
+			IDictionary<string, object> attributes = 
+				(gravatarAttributes == null
+					? new RouteValueDictionary()
+					: new RouteValueDictionary(gravatarAttributes));
+
+			var uriBuilder = new StringBuilder(GetGravatar(email));
+			bool first = true;
+
+			foreach (string key in attributes.Keys)
+			{
+				if (first)
+				{
+					uriBuilder.AppendFormat("?{0}={1}", key, attributes[key].ToString());
+					first = false;
+					continue;
+				}
+
+				uriBuilder.AppendFormat("&{0}={1}", key, attributes[key].ToString());
+			}
+
+			return uriBuilder.ToString();
+		}
+
+		private static string Hash(string Value)
+		{
+			var cryptoProvider = new MD5CryptoServiceProvider();
+
+			byte[] valueArray = Encoding.ASCII.GetBytes(Value);
+			valueArray = cryptoProvider.ComputeHash(valueArray);
+			
+			var hashBuilder = new StringBuilder();
+			foreach (var value in valueArray)
+				hashBuilder.Append(value.ToString("x2").ToLower());
+
+			return hashBuilder.ToString();
+		}
+	}
+}
