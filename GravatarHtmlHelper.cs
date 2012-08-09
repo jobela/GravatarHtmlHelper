d407c97cc0939e47430ade970fa40f56719db485
Joar Langaard <joar.langaard@gmail.com>
2012-08-09 17:16:43 +0200

    Added support for secure connection.
    Added support for secure connections. Added "class=gravatar" attribute
    to img element.

--- GravatarHtmlHelper.cs
+++ GravatarHtmlHelper.cs
@@ -7,21 +7,42 @@
 	using System.Web.Mvc;
 	using System.Web.Routing;
 
+	/// <summary>
+	/// Globally Recognised Avatar - Gravatar (http://gravatar.com)
+	/// </summary>
+	/// <remarks>
+	/// Written by Joar Langaard (https://github.com/jobela/GravatarHtmlHelper)
+	/// </remarks>
 	public static class GravatarHtmlHelper
 	{
+		/// <summary>
+		/// Returns a gravatar as an &lt;img /&gt;
+		/// </summary>
+		/// <param name="email">Email for the Gravatar</param>
 		public static HtmlString Gravatar(this HtmlHelper html, string email)
 		{
-			return GetImage(GetGravatar(email));
+			return GetImage(GetGravatar(html, email));
 		}
 
+		/// <summary>
+		/// Returns a gravatar as an &lt;img /&gt;
+		/// </summary>
+		/// <param name="email">Email for the Gravatar</param>
+		/// <param name="gravatarAttributes">An object that contains the Gravatar attributes for the element. ( s for size i pixels (default = 80), r for rating (default = G)) - http://nb.gravatar.com/site/implement/images/ </param>
 		public static HtmlString Gravatar(this HtmlHelper html, string email, object gravatarAttributes)
 		{
-			return GetImage(GetGravatar(email, gravatarAttributes));
+			return GetImage(GetGravatar(html, email, gravatarAttributes));
 		}
 
+		/// <summary>
+		/// Returns a gravatar as an &lt;img /&gt;
+		/// </summary>
+		/// <param name="email">Email for the Gravatar</param>
+		/// <param name="gravatarAttributes">An object that contains the Gravatar attributes for the element. ( s for size i pixels (default = 80), r for rating (default = G)) - http://nb.gravatar.com/site/implement/images/ </param>
+		/// <param name="htmlAttributes">An object that contains the HTML attributes for the element.</param>
 		public static HtmlString Gravatar(this HtmlHelper html, string email, object gravatarAttributes, object htmlAttributes)
 		{
-			return GetImage(GetGravatar(email, gravatarAttributes), htmlAttributes);
+			return GetImage(GetGravatar(html, email, gravatarAttributes), htmlAttributes);
 		}
 
 		private static HtmlString GetImage(string source)
@@ -39,24 +60,30 @@
 			var tagBuilder = new TagBuilder("img");
 			tagBuilder.MergeAttribute("src", source);
 			tagBuilder.MergeAttribute("alt", "Gravatar");
+			tagBuilder.MergeAttribute("class", "gravatar");
 			tagBuilder.MergeAttributes(attributes);
 
 			return new HtmlString(tagBuilder.ToString(TagRenderMode.SelfClosing));
 		}
 
-		private static string GetGravatar(string email)
+		private static string GetGravatar(HtmlHelper html, string email)
 		{
-			return string.Format("http://www.gravatar.com/avatar/{0}", GetHash(email));
+			/// Standard format http://www.gravatar.com/avatar/{0}
+			/// Secure format https://secure.gravatar.com/avatar/{0}
+			return string.Format("{0}://{1}.gravatar.com/avatar/{2}",
+				html.ViewContext.HttpContext.Request.IsSecureConnection ? "https" : "http",
+				html.ViewContext.HttpContext.Request.IsSecureConnection ? "secure" : "www",
+				GetHash(email));
 		}
 
-		private static string GetGravatar(string email, object gravatarAttributes)
+		private static string GetGravatar(HtmlHelper html, string email, object gravatarAttributes)
 		{
 			IDictionary<string, object> attributes =
 				(gravatarAttributes == null
 					? new RouteValueDictionary()
 					: new RouteValueDictionary(gravatarAttributes));
 
-			var uriBuilder = new StringBuilder(GetGravatar(email));
+			var uriBuilder = new StringBuilder(GetGravatar(html, email));
 			bool first = true;
 
 			foreach (string key in attributes.Keys)
@@ -74,6 +101,9 @@
 			return uriBuilder.ToString();
 		}
 
+		/// <summary>
+		/// Generates an MD5 hash of the string
+		/// </summary>
 		private static string GetHash(string email)
 		{
 			var cryptoProvider = new MD5CryptoServiceProvider();
